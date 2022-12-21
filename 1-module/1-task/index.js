3783f56d8cfc693a5303af607055d09e1944d38e
KristinaPreis <kristina.preys@gmail.com>
2022-12-22 01:22:17 +0600

    Created a function which counts factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,14 @@
 function factorial(n) {
-  // ваш код...
+  if (n == 0 || n == 1) {
+    let answer = 1;
+    return answer
+  }
+  if (n < 0) {
+    return 'Can\'t count factorial for a negative number'
+  }
+  let answer = n;
+  for (let i = 1; i < n; i++) {
+    answer *= (n - i);
+  }
+  return answer;
 }
