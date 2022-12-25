be2dcee1d42f2b95a1743238ade8b7f29e59d0f9
KristinaPreis <kristina.preys@gmail.com>
2022-12-26 01:06:09 +0600

    Created function which sums salaries

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,20 @@
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+}
+
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let salary in salaries) {
+    if (typeof salaries[salary] == 'number') {
+      if (isFinite(salaries[salary])) {
+        sum += salaries[salary];
+      }
+    }
+  }
+  return sum;
 }
